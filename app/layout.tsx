88e184811d3da147ec4b05e5d369786bd294844d
NiketGirdhar22 <niketgirdhar2004@gmail.com>
2025-01-30 04:26:16 +0530

    try open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,20 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
+
+  openGraph: {
+    title: 'Niket Girdhar',
+    description: 'Niket Girdhar - NT',
+    url: 'https://www.niketgirdhar.in',
+    images: [
+      {
+        url: '/favicon.png',
+        width: 1200,
+        height: 630,
+        alt: 'Niket Girdhar',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
