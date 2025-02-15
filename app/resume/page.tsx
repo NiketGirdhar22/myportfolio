ca64ef636fe9aa8d3e0567ff6f9acc861e56cb95
NiketGirdhar22 <niketgirdhar2004@gmail.com>
2025-02-15 14:11:55 +0530

    image add and restapi post

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,17 +1,31 @@
 import { Button } from '@/components/ui/button'
 import RedirectButton from '@/components/RedirectButton'
+import Image from 'next/image'
+import authorImage from '@/public/images/authors/niket.png'
 
 export default function Resume() {
   return (
     <section className="pb-24 pt-32">
       <div className="container max-w-3xl">
 
-        <section className="mt-8">
-          <h2 className="text-2xl font-semibold">About Me</h2>
-          <p className="mt-4 font-light text-muted-foreground">
-          A highly motivated and skilled Computer and Electronics Engineering student with a solid foundation in both hardware and software systems. Demonstrated expertise in web development, and programming, coupled with a strong passion for innovative technological solutions. Experienced in managing promotional campaigns for collage clubs and developing functional, service-oriented websites. Recognized for exceptional problem-solving abilities, meticulous attention to detail, and a collaborative approach to cross-disciplinary projects.
-          </p>
-        </section>
+    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+      <div className='mt-2 flex-1 md:mt-0'>
+        <h1 className='title no-underline'>About Me</h1>
+        <p className='mt-3 font-light text-muted-foreground text-justify'>
+        A highly motivated and skilled Computer and Electronics Engineering student with a solid foundation in both hardware and software systems. Demonstrated expertise in web development, and programming, coupled with a strong passion for innovative technological solutions. Experienced in managing promotional campaigns for collage clubs and developing functional, service-oriented websites. Recognized for exceptional problem-solving abilities, meticulous attention to detail, and a collaborative approach to cross-disciplinary projects.
+        </p>
+      </div>
+      <div className='relative'>
+        <Image
+            className='flex-1 rounded-lg grayscale'
+            src={authorImage}
+            alt='Niket Girdhar'
+            width={175}
+            height={175}
+            priority
+        />
+      </div>
+    </section>
 
         <section className="mt-12">
           <h2 className="text-2xl font-semibold">Skills</h2>
